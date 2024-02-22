9b3811f8cd116d4ec0067b6949f3c1e2447a9531
Bhavya2662 <bhavyaas2662@hotmail.com>
2024-02-22 21:49:10 +0530

    included test

--- src/main.rs
+++ src/main.rs
@@ -1,122 +1,125 @@
-#![allow(unused_imports)]
-use num_bigint::BigUint;
-use rand::Rng;
-use num_traits::Num;
-use num_traits::cast::FromPrimitive;
-use num_traits::identities::One;
+use curv::cryptographic_primitives::proofs::sigma_dlog::DLogProof;
+use curv::elliptic::curves::{secp256_k1::Secp256k1, Point, Scalar};
+use curv::BigInt;
+use paillier::{DecryptionKey, EncryptionKey, Paillier, Randomness, RawCiphertext, RawPlaintext};
 
-use num_traits::ToPrimitive;
-use rand::distributions::{Distribution, Uniform};
-use lazy_static::lazy_static;
-use std::ops::Rem;
-use num_traits::CheckedSub;
-
-
-//const P: BigUint = BigUint::parse_bytes(b"7919", 10).unwrap(); // Prime modulus (configurable)
-//const Q: BigUint = BigUint::parse_bytes(b"8000", 10).unwrap(); // Subgroup order (configurable)
-
-lazy_static! {
-    static ref P: BigUint = BigUint::parse_bytes(b"7919", 10).unwrap(); // Prime modulus (configurable)
-    static ref Q: BigUint = BigUint::parse_bytes(b"8000", 10).unwrap(); // Subgroup order (configurable)
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct MessageA {
+  pub c: BigInt,           // paillier encryption
 }
 
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct MessageB {
+  pub c: BigInt, // paillier encryption
+}
+
+impl MessageA {
+  /// Creates a new `messageA` using Alice's Paillier encryption key.
+  pub fn a(
+    a: &Scalar<Secp256k1>,
+    alice_ek: &EncryptionKey,
+  ) -> (Self, BigInt) {
+    let randomness = BigInt::sample_below(&alice_ek.n);
+    let m_a = MessageA::a_with_predefined_randomness(a, alice_ek, &randomness);
+    (m_a, randomness)
+  }
+  pub fn a_with_predefined_randomness(
+    a: &Scalar<Secp256k1>,
+    alice_ek: &EncryptionKey,
+    randomness: &BigInt,
+  ) -> Self {
+    let c_a = Paillier::encrypt_with_chosen_randomness(
+      alice_ek,
+      RawPlaintext::from(a.to_bigint()),
+      &Randomness::from(randomness.clone()),
+    )
+    .0
+    .clone()
+    .into_owned();
+    Self {
+      c: c_a,
+    }
+  }
+}
+
+impl MessageB {
+  pub fn b(
+    b: &Scalar<Secp256k1>,
+    alice_ek: &EncryptionKey,
+    m_a: MessageA,
+  ) -> Result<(Self, Scalar<Secp256k1>, BigInt, BigInt), Error> {
+    let beta_tag = BigInt::sample_below(&alice_ek.n);
+    let randomness = BigInt::sample_below(&alice_ek.n);
+    let (m_b, beta) = MessageB::b_with_predefined_randomness(
+      b,
+      alice_ek,
+      m_a,
+      &randomness,
+      &beta_tag,
+    )?;
+
+    Ok((m_b, beta, randomness, beta_tag))
+  }
+  pub fn b_with_predefined_randomness(
+    b: &Scalar<Secp256k1>,
+    alice_ek: &EncryptionKey,
+    m_a: MessageA,
+    randomness: &BigInt,
+    beta_tag: &BigInt,
+  ) -> Result<(Self, Scalar<Secp256k1>), Error> {
+    let beta_tag_fe = Scalar::<Secp256k1>::from(beta_tag);
+    let c_beta_tag = Paillier::encrypt_with_chosen_randomness(
+      alice_ek,
+      RawPlaintext::from(beta_tag),
+      &Randomness::from(randomness.clone()),
+    );
+
+    let b_bn = b.to_bigint();
+    let b_c_a = Paillier::mul(
+      alice_ek,
+      RawCiphertext::from(m_a.c),
+      RawPlaintext::from(b_bn),
+    );
+    let c_b = Paillier::add(alice_ek, b_c_a, c_beta_tag);
+    let beta = Scalar::<Secp256k1>::zero() - &beta_tag_fe;
+    Ok((
+      Self {
+        c: c_b.0.clone().into_owned(),
+      },
+      beta,
+    ))
+  }
+
+  pub fn verify_proofs_get_alpha(
+    &self,
+    dk: &DecryptionKey,
+    a: &Scalar<Secp256k1>,
+  ) -> Result<(Scalar<Secp256k1>, BigInt), Error> {
+    let alice_share = Paillier::decrypt(dk, &RawCiphertext::from(self.c.clone()));
+    let alpha = Scalar::<Secp256k1>::from(alice_share.0.as_ref());
+    if alpha * a == Scalar::<Secp256k1>::one() {
+      Ok((alpha, alice_share.0.into_owned()))
+    } else {
+      Err(InvalidKey)
+    }
+  }
+}
+
+pub(crate) fn generate_init() -> (EncryptionKey, DecryptionKey) {
+  let (ek, dk) = Paillier::keypair().keys();
+  (ek, dk)
+}
 fn main() {
-    // Get Alice's and Bob's secrets (simulated for a test case)
-    let a = BigUint::from(1234u32);
-    let b = BigUint::from_u64(5678u64).unwrap();
-    //let b = BigUint::cast(5678u64);
+  let alice_input = Scalar::<Secp256k1>::random();
+  let (ek_alice, dk_alice) = generate_init();
+  let bob_input = Scalar::<Secp256k1>::random();
+  let (m_a, _) = MessageA::a(&alice_input, &ek_alice);
+  let (m_b, beta, _, _) = MessageB::b(&bob_input, &ek_alice, m_a).unwrap();
+  let alpha = m_b
+    .verify_proofs_get_alpha(&dk_alice, &alice_input)
+    .expect("wrong dlog or m_b");
 
-     // Print shares
-     println!("(preMTA)- Alice's share (a): {:?}", &a);
-     println!("(preMTA)- Bob's share (b): {:?}", &b);
-
-
-    // Perform MtA protocol
-    let (alpha, beta) = mta_protocol(&a, &b);
-    dbg!(&alpha+&beta);
-
-    // Print shares
-    println!("Alice's share (alpha): {:?}", alpha);
-    println!("Bob's share (beta): {:?}", beta);
-
-    // Reconstruct and verify the secret
-    let reconstructed_secret = reconstruct_secret(alpha, beta).unwrap();
-    dbg!((&a*&b)%Q.clone() );
-    
-    assert_eq!(reconstructed_secret, a * b % Q.clone() , "Reconstructed secret does not match");
-    println!("Reconstructed secret: {}", reconstructed_secret);
-}
-
-fn mta_protocol(a: & BigUint, b: & BigUint) -> (BigUint, BigUint) {
-    // Generate Alice's RSA key pair (using a simplified modulus for clarity)
-    //let (e, d) = (BigUint::from(65537u32), BigUint::cast(0x2a2b5143d8c0956cu64));
-    let (e, d) = (BigUint::from(65537u32), BigUint::from_u64(0x2a2b5143d8c0956cu64).unwrap());
-
-    fn encrypt_a(
-        e: &BigUint,
-        message: &BigUint
-    ) -> BigUint {
-        message.modpow(&e, &P.clone())
-    }
-
-    fn decrypt_a(
-        d: &BigUint,
-        ciphertext: BigUint
-    ) -> BigUint {
-        ciphertext.modpow(&d, &P.clone())
-    }
-
-    let mut rng = rand::thread_rng();
-
-    // Alice computes cA = Encrypt_A(a)
-    let ca = encrypt_a(&e, &a);
-
-    // Bob selects β′ <– ZN
-    //let beta_prime = rng.gen_range(BigUint::one(), Q.clone());
-
-    let q_u64 = Q.to_u64().expect("Q value too large for u64");
-    let one = BigUint::one();
-
-    let range = Uniform::new_inclusive(one.to_u64().unwrap(), q_u64);
-    let beta_prime_u64 = range.sample(&mut rng);
-
-    let beta_prime = BigUint::from_u64(beta_prime_u64).unwrap();
-    // Bob computes cB = b * cA + Encrypt_A(β′) = Encrypt_A(ab+β′)
-    //let cb = (b * ca + encrypt_a(&e, beta_prime)) % P;
-
-    let cb = (b * ca + encrypt_a(&e, &beta_prime)) % P.clone();
-
-
-    // Bob sets additive share β = -β′ mod q
-    //let beta = (*Q - beta_prime) % *Q;
-    let beta = Q.clone() - (beta_prime % Q.clone());
-    //let beta = (Q.checked_sub(&beta_prime).unwrap_or_else(|| Q.clone() - &beta_prime)) % Q.clone();
-
-
-    // Bob sends cB to Alice
-    // Bob would send cB over a secure communication channel.
-    // For this simplified example, we assume they are in the same environment.
-    println!("Bob sends cB to Alice: {}", cb);
-
-    // Alice decrypts α' = dec(cB)
-    let alpha_prime = decrypt_a(&d, cb);
-
-    // Alice sets α = α′ mod q
-    let alpha = alpha_prime % Q.clone();
-
-    (alpha, beta)
-}
-
-fn reconstruct_secret(alpha: BigUint, beta: BigUint) -> Option<BigUint> {
-    // Verify shares are within correct range
-    if alpha >= *Q || beta >= *Q {
-        return None;
-    }
-
-    //dbg!(&alpha);
-    //dbg!(&beta);
-
-    // Reconstruct secret
-    Some((alpha * beta)% Q.clone())
-
+  let left = alpha.0 + beta;
+  let right = alice_input * bob_input;
+  assert_eq!(left, right);
 }
